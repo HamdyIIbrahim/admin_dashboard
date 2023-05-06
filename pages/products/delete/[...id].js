467fe79fc9b52da5a0b4b783fcc2d1539ae6b7b7
HamdyIIbrahim <hamdyfarouk444@gmail.com>
2023-05-07 02:52:54 +0300

    update undifiend reading in the delete product component

--- pages/products/delete/[...id].js
+++ pages/products/delete/[...id].js
@@ -7,7 +7,7 @@ export default function DeleteProduct() {
   const [product, setProduct] = useState(null);
   const router = useRouter();
   const ID = router.query.id;
-  const id = ID[0];
+  const id = ID?ID[0]:null;
   useEffect(() => {
     axios
       .get(`https://admin-dashboard-backend-rnc4.onrender.com/products/${id}`)
